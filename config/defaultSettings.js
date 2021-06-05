547d3507f327f8b61bbf7d61575cbffb62bdb296
Jo1nson <626837428@qq.com>
2021-06-05 08:54:42 +0800

    初始化商城管理项目

---
+++ config/defaultSettings.js
@@ -0,0 +1,14 @@
+const proSettings = {
+  navTheme: 'dark',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'side',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: '融职商城',
+  pwa: false,
+  iconfontUrl: '',
+};
+export default proSettings;
---
+++ config/routes.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    path: '/',
+    component: '../layouts/BlankLayout',
+    routes: [
+      {
+        path: '/login',
+        component: '../layouts/LoginLayout',
+        routes: [
+          {
+            name: 'login',
+            path: '/login',
+            component: './Login',
+          },
+        ],
+      },
+      {
+        path: '/',
+        component: '../layouts/SecurityLayout',
+        routes: [
+          {
+            path: '/',
+            component: '../layouts/BasicLayout',
+            routes: [
+              {
+                path: '/',
+              },
+              {
+                component: './404',
+              },
+            ],
+          },
+          {
+            component: './404',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    component: './404',
+  },
+];
